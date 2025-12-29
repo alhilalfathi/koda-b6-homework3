6dd5c63cab1a59807959af08fde2f138496083aa
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2025-12-29 23:26:27 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const dataNama = ["joni","jane","john","mikael","mikaya","Mamika"]
+
+
+
+// function searchStudent (nama,countResult){
+//     for(i=0; i<=dataNama.length-1; i++){
+//         for(j=0; j<=dataNama[i]; j++){
+//             console.log(dataNama[i][j])
+//         }
+        
+//     }
+
+// }
+// searchStudent("joni")
+// let dataSet = []
+// for(i=0; i<=dataNama.length-1; i++){
+//         for(j=0; j<=dataNama[i].length-1; j++){
+//             console.log(dataNama[i][j])
+//             dataSet.push(dataNama[i][j])
+//             // dataSet += dataNama[i][j]
+
+            
+//         }
+//         console.log("--------------")
+//     }
+// console.log(dataSet)
