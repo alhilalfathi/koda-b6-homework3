07350a82a3d4e49a6471a6c67cefd7e79ca8f7bd
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2025-12-30 23:13:59 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,82 @@
-const dataNama = ["joni","jane","john","mikael","mikaya","Mamika"]
 
-let found = []
+const dataNama = ["joni","jane","john","mikael","Jono","mikaya","Mamika","joko"]
 
-function searchStudent (nama,countResult){
-    for(let i=0; i<dataNama.length; i++){
-        for(let j=0; j<=dataNama[i].length-1; j++){
-            // console.log(dataNama[i][j])
-            // console.log("---"+nama[0])
-            if (dataNama[i][j] === nama [0]){
-                for (let k=0; k<nama.length; k++){
-                    if (dataNama[i][j+k]===nama[k]){
-                        // console.log(nama[k])
-                        found[i]=dataNama[i]
-                        // console.log(found)
-                    }
-                }
-            }
-        }
-        
-    } console.log(found)
+const hasil = []
 
+const lowerCaseMap = {
+    A: "a",
+    B: "b",
+    C: "c",
+    D: "d",
+    F: "f",
+    G: "g",
+    H: "h",
+    I: "i",
+    J: "j",
+    K: "k",
+    L: "l",
+    N: "n",
+    O: "o",
+    P: "p",
+    Q: "q",
+    R: "r",
+    S: "s",
+    T: "t",
+    U: "u",
+    V: "v",
+    W: "w",
+    X: "x",
+    Y: "y",
+    Z: "z"
 }
-// searchStudent("joni")
-// let dataSet = []
-// for(i=0; i<dataNama.length; i++){
-//         for(j=0; j<dataNama[i].length; j++){
-//             console.log(dataNama[i][j])
-//             // dataSet.push(dataNama[i][j])
-//             // dataSet += dataNama[i][j]
-            
-            
-//         }
-        
-//     }
-searchStudent("joni")
-// console.log(dataSet)
+function toLowerManual(str) {
+  let hasil = ""
+  for (let i=0; i<str.length; i++) {
+    if (lowerCaseMap[str[i]]) {
+      hasil += lowerCaseMap[str[i]]
+    } else {
+      hasil += str[i]
+    }
+  }
+  return hasil
+}
+
+
+function searchNama(keyword, cb) {
+  
+  const keyLow = toLowerManual(keyword)
+
+  for (let a=0; a<dataNama.length; a++) {
+    let nama = dataNama[a]
+    let cocok = false
+
+    for (let i=0; i<=nama.length - keyLow.length; i++) {
+      let sama = true
+
+      for (let j=0; j<keyLow.length; j++) {
+        let hurufNama = lowerCaseMap[nama[i + j]] || nama[i + j]
+        if (hurufNama !== keyLow[j]) {
+          sama = false
+          break
+        }
+      }
+
+      if (sama) {
+        cocok = true
+        break
+      }
+    }
+
+    if (cocok) {
+      hasil.push(nama)
+    }
+  }
+  cb()
+  return hasil
+}
+function counter(){
+    console.log(hasil.length)
+    
+}
+
+console.log(searchNama("JoN", counter))
