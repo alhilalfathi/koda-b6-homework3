5887a599fa2555ea34debe86925675096df393e1
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2025-12-30 08:29:28 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dataNama = ["joni","jane","john","mikael","mikaya","Mamika"]
 
-
+let found = []
 
 function searchStudent (nama,countResult){
     for(let i=0; i<dataNama.length; i++){
@@ -10,13 +10,15 @@ function searchStudent (nama,countResult){
             if (dataNama[i][j] === nama [0]){
                 for (let k=0; k<nama.length; k++){
                     if (dataNama[i][j+k]===nama[k]){
-                        console.log(nama[k])
+                        // console.log(nama[k])
+                        found[i]=dataNama[i]
+                        // console.log(found)
                     }
                 }
             }
         }
         
-    }
+    } console.log(found)
 
 }
 // searchStudent("joni")
